6d5fadecbc5596b868e6d831a76bbbbb90c06ff3
Tuan Tu Tran <tu.tt@financialdeepmind.com>
2023-03-19 19:42:29 -0400

    add the case of an empty input string

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,7 +1,13 @@
 package main
+
 import "fmt"
+
 const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name
 }
 
